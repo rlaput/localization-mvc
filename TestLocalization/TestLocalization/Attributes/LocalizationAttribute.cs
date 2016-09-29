cb8314da089b5d957db95b949c5de740897ac738
Royce Beryl Laput <royce.laput@gmail.com>
2016-09-29 08:50:04 -0500

    initial commit

---
+++ TestLocalization/TestLocalization/Attributes/LocalizationAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestLocalization.Attributes
+{
+    public class LocalizationAttribute : ActionFilterAttribute
+    {
+        private string _DefaultLanguage = "en";
+
+        public LocalizationAttribute(string defaultLanguage)
+        {
+            this._DefaultLanguage = defaultLanguage;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string lang = (string)filterContext.RouteData.Values["lang"];
+            if (lang == null)
+            {
+                string[] browserLanguages = filterContext.RequestContext.HttpContext.Request.UserLanguages;
+                lang =
+                    (browserLanguages != null && browserLanguages.Length > 0) ?
+                    browserLanguages[0] : _DefaultLanguage;
+            }
+
+            try
+            {
+                Thread.CurrentThread.CurrentCulture =
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(lang);
+            }
+            catch (Exception e)
+            {
+                throw new NotSupportedException(String.Format("ERROR: Invalid language code '{0}'.", lang));
+            }
+        }
+    }
+}
---
+++ TestLocalization/TestLocalization/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestLocalization.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = Resources.About.TextAboutCaption1;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = Resources.Contact.TextContactCaption;
+
+            return View();
+        }
+    }
+}
